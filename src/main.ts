3921f50cc6c30bd0f6c489824df79a597c256cce
Javier Montes <javier.montes.carrera@gmail.com>
2023-10-18 13:18:32 +0800

    Switching to createAndMint function
    Switching to createAndMint function in order to use just one function to create the token and mint it as stated

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { 
     mplTokenMetadata,
-    createV1,
     TokenStandard,
-    mintV1,
-    createFungible,
+    // mintV1,
+    // createFungible,
+    createAndMint,
  } from "@metaplex-foundation/mpl-token-metadata";
 import { 
     keypairIdentity,
@@ -21,50 +21,53 @@ umi.use(keypairIdentity(keypair))
     .use(mplTokenMetadata())
 
 const metadata = {
-    name: "Platinum Solana",
+    name: "Platinum Solana (PSOL)",
     symbol: "PSOL",
     uri: "https://javiermontescarrera.github.io/solana-demos/res/tokens/demo-spl-token.json",
 };
 
 const mint = generateSigner(umi);
-async function createMetadataDetails() {
-    await createV1(umi, {
-        mint,
-        authority: umi.identity,
-        name: metadata.name,
-        symbol: metadata.symbol,
-        uri: metadata.uri,
-        sellerFeeBasisPoints: percentAmount(0),
-        decimals: 0,
-        tokenStandard: TokenStandard.Fungible,
-    }).sendAndConfirm(umi)
-}
 
-async function mintToken() {
-    await mintV1(umi, {
-        mint: mint.publicKey,
-        authority: umi.identity,
-        amount: 10_000,
-        tokenOwner: umi.identity.publicKey,
-        tokenStandard: TokenStandard.Fungible,
-    }).sendAndConfirm(umi)
-}
+// async function mintToken() {
+//     await mintV1(umi, {
+//         mint: mint.publicKey,
+//         authority: umi.identity,
+//         amount: 10_000,
+//         tokenOwner: umi.identity.publicKey,
+//         tokenStandard: TokenStandard.Fungible,
+//     }).sendAndConfirm(umi)
+// }
 
 console.log(mint);
 console.log("Mint publicKey: ", mint.publicKey); 
 
-createMetadataDetails().then(() => {
-    createFungible(umi, {
-        mint,
-        authority: umi.identity,
-        name: metadata.name,
-        symbol: metadata.symbol,
-        uri: metadata.uri,
-        sellerFeeBasisPoints: percentAmount(0),
-        decimals: 0,
-    }).sendAndConfirm(umi).then(() => {
-        mintToken().then(() => {
-            console.log("10,000 PSOL (", mint.publicKey, ") minted");
-        });
-    });
+// createFungible(umi, {
+//     mint,
+//     authority: umi.identity,
+//     name: metadata.name,
+//     symbol: metadata.symbol,
+//     uri: metadata.uri,
+//     sellerFeeBasisPoints: percentAmount(0),
+//     decimals: 0,
+// }).sendAndConfirm(umi).then(() => {
+//     mintToken().then(() => {
+//         console.log("10,000 PSOL (", mint.publicKey, ") minted");
+//     });
+// });
+
+let amount_to_mint:number = 10_000;
+
+createAndMint(umi, {
+    mint,
+    authority: umi.identity,
+    name: metadata.name,
+    symbol: metadata.symbol,
+    uri: metadata.uri,
+    sellerFeeBasisPoints: percentAmount(0),
+    decimals: 3,
+    amount: amount_to_mint,
+    tokenOwner: umi.identity.publicKey,
+    tokenStandard: TokenStandard.Fungible,
+}).sendAndConfirm(umi).then(() => {
+    console.log("10 PSOL (", mint.publicKey, ") minted");
 });
